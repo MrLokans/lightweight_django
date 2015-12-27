1dd66677793e75edbaf3085263d2e60c2dedcf49
MrLokans <trikster1911@gmail.com>
2015-12-27 09:00:48 +0300

    Model fixes

--- scrum/board/models.py
+++ scrum/board/models.py
@@ -30,7 +30,7 @@ class Task(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
     sprint = models.ForeignKey(Sprint, blank=True, null=True)
-    status = models.SmallIntegerField(chocies=STATUS_CHOICES, default=STATUS_TODO)
+    status = models.SmallIntegerField(choices=STATUS_CHOICES, default=STATUS_TODO)
     order = models.SmallIntegerField(default=0)
     assigned = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True)
     started = models.DateField(blank=True, null=True)
