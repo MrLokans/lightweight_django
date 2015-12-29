9e0635ba9b9f5f7b0e0717f05de2fe031556d7be
MrLokans <trikster1911@gmail.com>
2015-12-29 15:49:47 +0300

    Extra filtering

--- scrum/board/forms.py
+++ scrum/board/forms.py
@@ -1,10 +1,32 @@
 import django_filters
 
+from django.contrib.auth import get_user_model
+
 from .models import Task
+
+
+User = get_user_model()
+
+
+class NullFilter(django_filters.BooleanFilter):
+
+    def filter(self, qs, value):
+        # /api/tasks/?backlog=True will return aall tasks that are not assigned to a sprint
+        if value is not None:
+            return qs.filter(**{"{}__isnull".format(self.name): value})
+        return qs
 
 
 class TaskFilter(django_filters.FilterSet):
 
+    backlog = NullFilter(name='sprint')
+
     class Meta:
         model = Task
-        fields = ('sprint', 'status', 'assigned', )
+        fields = ('sprint', 'status', 'assigned', 'backlog', )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # /api/tasks/?assigned=<username>
+        self.filters['assigned'].extra.update(
+            {'to_field_name': User.USERNAME_FIELD})
