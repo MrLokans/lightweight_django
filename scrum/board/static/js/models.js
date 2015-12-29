999a7d13faf35a92e1faedf6fb97c895580997cf
MrLokans <trikster1911@gmail.com>
2015-12-29 16:37:17 +0300

    backbone specific setup.

---
+++ scrum/board/static/js/models.js
@@ -0,0 +1,3 @@
+// Backbone models are where the data of the application is defined and state manip‐
+// ulation occurs. In this file we’ll be handling the CSRF (cross-site request forgery)
+// token and the user session management
---
+++ scrum/board/static/js/views.js
@@ -0,0 +1,3 @@
+// Each Backbone view will consist of the different “pages” we will need for the user
+// to easily navigate around the application. These views will also include form data
+// we’ll need for updating sprints and tasks for our Scrum board.
