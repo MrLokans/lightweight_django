4e09120ceb9cdf92c112427c714073222239b8ce
MrLokans <trikster1911@gmail.com>
2015-12-30 18:23:45 +0300

    Added Backbone's homepage view.

--- scrum/board/static/board/js/views.js
+++ scrum/board/static/board/js/views.js
@@ -1,3 +1,22 @@
 // Each Backbone view will consist of the different “pages” we will need for the user
 // to easily navigate around the application. These views will also include form data
 // we’ll need for updating sprints and tasks for our Scrum board.
+
+(function($, Backbone, _, app){
+    var HomepageView = Backbone.View.extend({
+        templateName: '#home-template',
+        initialize: function(){
+            this.template = _.template($(this.templateName).html());
+        },
+        render: function(){
+            var context = this.getContext();
+            var html = this.template(context);
+            this.$el.html(html);
+        },
+        getContext: function(){
+            return {};
+        }
+    });
+
+    app.views.HomepageView = HomepageView;
+})(jQuery, Backbone, _, app);
