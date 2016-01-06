a4429380492d666462cdb33f600604c5127f3202
MrLokans <trikster1911@gmail.com>
2016-01-06 17:17:07 +0300

    Added login backbone view.

--- scrum/board/static/board/js/views.js
+++ scrum/board/static/board/js/views.js
@@ -3,6 +3,22 @@
 // we’ll need for updating sprints and tasks for our Scrum board.
 
 (function($, Backbone, _, app){
+
+    var LoginView = Backbone.View.extend({
+        id: 'login',
+        templateName: '#login-template',
+        initialize: function(){
+            this.template = _.template($(this.templateName).html());
+        },
+        render: function(){
+            var context = this.getContext();
+            var html = this.template(context);
+            this.$el.html(html);
+        },
+        getContext: function(){
+            return {};
+        }
+    });
     var HomepageView = Backbone.View.extend({
         templateName: '#home-template',
         initialize: function(){
